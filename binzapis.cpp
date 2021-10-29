2a5e582576b8ea977bbfa6963252069ea1b748f1
David Toholj <davidtoholj0101@gmail.com>
2021-10-29 22:39:05 +0200

    binary.cpp
    Print out binary digits of a natural number n.

--- binzapis.cpp
+++ binzapis.cpp
@@ -7,15 +7,15 @@ int main()
 {
 	uint64_t n;
 	cin >> n;
-	vector<int> bin(32);
+	vector<int> binary(32);
 	int i = 0;
 	while(n)
 	{
-		bin[i++] = n % 2;
+		binary[i++] = n % 2;
 		n /= 2;
 	}
 	for(i = 31; i >= 0; i--)
-		cout << bin[i];
+		cout << binary[i];
 	cout << endl;
 	return 0;
 }
